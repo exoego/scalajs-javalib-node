eb7e047e69d73888bf7be68abb89be77339d8eb4
exoego <mogami@exoego.net>
2019-06-05 12:26:38 +0900

    Implement CharArrayReader

--- jdk/js/src/main/scala/java/io/CharArrayReader.scala
+++ jdk/js/src/main/scala/java/io/CharArrayReader.scala
@@ -1,50 +1,96 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package java.io
 
-class CharArrayReader protected (
-    val buf: Array[Char],
-    val count: Int,
-    var markedPos: Int,
-    var pos: Int
-) extends Reader {
+class CharArrayReader(protected var buf: Array[Char], offset: Int, length: Int) extends Reader {
+  if (offset < 0 || offset > buf.length || length < 0 || offset + length < 0)
+    throw new IllegalArgumentException
 
-  private[this] var closed = false
+  protected var pos: Int       = offset
+  protected var markedPos: Int = offset
+  protected var count: Int     = if (offset + length < buf.length) length else buf.length
 
   def this(buf: Array[Char]) {
-    this(buf, buf.length, 0, 0)
+    this(buf, 0, buf.length)
   }
 
-  def this(buf: Array[Char], offset: Int, length: Int) {
-    this(buf, buf.length, 0, 0)
+  @Override def close(): Unit = {
+    if (!isClosed) buf = null
   }
 
-  override def read(cbuf: Array[Char], off: Int, len: Int): Int = {
+  @inline private def isClosed = buf == null
+
+  override def mark(readLimit: Int): Unit = {
     ensureOpen()
+    markedPos = pos
+  }
 
-    if (off < 0 || len < 0 || len > cbuf.length - off) {
-      throw new IndexOutOfBoundsException
-    }
+  override val markSupported = true
 
-    if (len == 0) {
-      0
-    } else {
-      val count = Math.min(len, buf.length - pos)
-      var i     = 0
-      while (i < count) {
-        cbuf(off + i) = buf.charAt(pos + i)
-        i += 1
-      }
-      pos += count
-      if (count == 0) -1 else count
+  override def read: Int = {
+    ensureOpen()
+    if (pos == count) -1
+    else {
+      pos += 1
+      buf(pos - 1)
     }
   }
 
-  override def close(): Unit = {
-    this.closed = true
+  def read(buffer: Array[Char], offset: Int, len: Int): Int = {
+    if (offset < 0 || offset > buffer.length) throw new IndexOutOfBoundsException()
+    if (len < 0 || len > buffer.length - offset) throw new IndexOutOfBoundsException()
+    ensureOpen()
+    if (pos < this.count) {
+      val bytesRead =
+        if (pos + len > this.count) this.count - pos
+        else len
+      System.arraycopy(this.buf, pos, buffer, offset, bytesRead)
+      pos += bytesRead
+      bytesRead
+    } else {
+      -1
+    }
+  }
+
+  override def ready: Boolean = {
+    ensureOpen()
+    pos != count
+  }
+
+  override def reset(): Unit = {
+    ensureOpen()
+    pos = if (markedPos != -1) markedPos else 0
+  }
+
+  override def skip(n: Long): Long = {
+    ensureOpen()
+    if (n <= 0) return 0
+    var skipped = 0L
+    if (n < this.count - pos) {
+      pos = pos + n.toInt
+      skipped = n
+    } else {
+      skipped = this.count - pos
+      pos = this.count
+    }
+    skipped
   }
 
   private def ensureOpen(): Unit = {
-    if (this.closed)
-      throw new IOException("Operation on closed stream")
+    if (isClosed) throw new IOException()
   }
-
 }
---
+++ jdk/shared/src/test/scala/luni/java/io/CharArrayReaderTest.scala
@@ -0,0 +1,109 @@
+package luni.java.io
+
+import java.io.CharArrayReader
+import java.io.IOException
+
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+
+class CharArrayReaderTest extends FunSuite with BeforeAndAfterEach {
+  private[io] val hw                  = Array('H', 'e', 'l', 'l', 'o', 'W', 'o', 'r', 'l', 'd')
+  private[io] var cr: CharArrayReader = _
+
+  override def afterEach(): Unit = {
+    if (cr != null) cr.close()
+  }
+
+  test("Constructor$C") {
+    cr = new CharArrayReader(hw)
+    assert(cr.ready)
+  }
+
+  test("Constructor$CII") {
+    cr = new CharArrayReader(hw, 5, 5)
+    assert(cr.ready)
+    val c = cr.read
+    assert(c == 'W')
+  }
+
+  test("close") {
+    cr = new CharArrayReader(hw)
+    cr.close()
+    assertThrows[IOException] {
+      cr.read
+    }
+    // No-op
+    cr.close()
+  }
+
+  test("markI") {
+    cr = new CharArrayReader(hw)
+    cr.skip(5L)
+    cr.mark(100)
+    cr.read()
+    cr.reset()
+    assert('W' == cr.read)
+  }
+
+  test("markSupported") {
+    cr = new CharArrayReader(hw)
+    assert(cr.markSupported)
+  }
+
+  test("read") {
+    cr = new CharArrayReader(hw)
+    assert('H' == cr.read)
+    cr = new CharArrayReader(Array[Char]('\u8765'))
+    assert(cr.read == '\u8765')
+  }
+
+  test("read$CII"){
+    val c = new Array[Char](11)
+    cr = new CharArrayReader(hw)
+    cr.read(c, 1, 10)
+    assert(new String(c, 1, 10) == new String(hw, 0, 10))
+  }
+
+  test("ready") {
+    cr = new CharArrayReader(hw)
+    assert(cr.ready)
+    cr.skip(1000)
+    assert(!cr.ready)
+    cr.close()
+    assertThrows[IOException] {
+      cr.ready
+    }
+    cr = new CharArrayReader(hw)
+    cr.close()
+    assertThrows[IOException] {
+      cr.ready()
+    }
+  }
+
+  test("reset") {
+    cr = new CharArrayReader(hw)
+    cr.skip(5L)
+    cr.mark(100)
+    cr.read()
+    cr.reset()
+    assert('W' == cr.read)
+
+    // Regression for HARMONY-4357
+    val str  = "offsetHello world!"
+    val data = new Array[Char](str.length)
+    str.getChars(0, str.length, data, 0)
+    val offsetLength = 6
+    val length       = data.length - offsetLength
+    val reader       = new CharArrayReader(data, offsetLength, length)
+    reader.reset()
+    for (i <- 0 until length) {
+      assert(data(offsetLength + i) == reader.read.asInstanceOf[Char])
+    }
+  }
+
+  test("skipJ"){
+    cr = new CharArrayReader(hw)
+    val skipped = cr.skip(5L)
+    assert(5L == skipped)
+    assert('W' == cr.read)
+  }
+}
