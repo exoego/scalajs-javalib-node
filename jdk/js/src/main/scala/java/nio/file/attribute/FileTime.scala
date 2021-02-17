4ea28a1c860404a5414f9e15454e2b4b40aba9bb
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-02-17 20:01:46 +0900

    Implement FileTime

--- jdk/js/src/main/scala/java/nio/file/attribute/FileTime.scala
+++ jdk/js/src/main/scala/java/nio/file/attribute/FileTime.scala
@@ -1,3 +1,59 @@
 package java.nio.file.attribute
 
-final class FileTime {}
+import java.time.Instant
+import java.util.concurrent.TimeUnit
+import scalajs.js
+
+final class FileTime private (val nanos: Long) extends Comparable[FileTime] {
+  private lazy val hash = nanos.hashCode()
+  private lazy val stringRep =
+    new js.Date((nanos / 1000000L).toDouble).toISOString().replace(".000", "")
+
+  override def compareTo(o: FileTime): Int = {
+    if (this.nanos == o.nanos) {
+      0
+    } else if (this.nanos > o.nanos) {
+      1
+    } else {
+      -1
+    }
+  }
+
+  override def equals(obj: Any): Boolean =
+    obj match {
+      case other: FileTime => this.nanos == other.nanos
+      case _               => false
+    }
+
+  override def hashCode(): Int = hash
+
+  override def toString: String = stringRep
+
+  def to(unit: TimeUnit): Long = unit match {
+    case TimeUnit.NANOSECONDS  => nanos
+    case TimeUnit.MICROSECONDS => nanos / 1000L
+    case TimeUnit.MILLISECONDS => nanos / 1000000L
+    case TimeUnit.SECONDS      => nanos / 1000000000L
+    case TimeUnit.MINUTES      => nanos / 60000000000L
+    case TimeUnit.HOURS        => nanos / 3600000000000L
+    case TimeUnit.DAYS         => nanos / 86400000000000L
+  }
+
+  def toMillis(): Long = nanos / 1000000L
+}
+
+object FileTime {
+  def from(instant: Instant): FileTime = ???
+
+  def from(value: Long, unit: TimeUnit): FileTime = unit match {
+    case TimeUnit.NANOSECONDS  => new FileTime(value)
+    case TimeUnit.MICROSECONDS => new FileTime(value * 1000L)
+    case TimeUnit.MILLISECONDS => new FileTime(value * 1000000L)
+    case TimeUnit.SECONDS      => new FileTime(value * 1000000000L)
+    case TimeUnit.MINUTES      => new FileTime(value * 60000000000L)
+    case TimeUnit.HOURS        => new FileTime(value * 3600000000000L)
+    case TimeUnit.DAYS         => new FileTime(value * 86400000000000L)
+  }
+
+  def fromMillis(value: Long): FileTime = new FileTime(value * 1000000L)
+}
---
+++ jdk/shared/src/test/scala/luni/java/nio/files/attributes/FileTimeTest.scala
@@ -0,0 +1,145 @@
+package luni.java.nio.files.attributes
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import java.nio.file.attribute.FileTime
+import java.util.concurrent.TimeUnit._
+
+class FileTimeTest extends AnyFunSuite {
+  ignore("from(Instant)") {
+    // need scala-java-time
+  }
+
+  ignore("toInstant") {
+    // need scala-java-time
+  }
+
+  test("from(Long,TimeUnit)") {
+    assert(FileTime.from(1, NANOSECONDS).to(NANOSECONDS) === 1L)
+    assert(FileTime.from(1, NANOSECONDS).to(MICROSECONDS) === 0L)
+
+    assert(FileTime.from(1, MICROSECONDS).to(NANOSECONDS) === 1000L)
+    assert(FileTime.from(1, MICROSECONDS).to(MICROSECONDS) === 1L)
+    assert(FileTime.from(1, MICROSECONDS).to(MILLISECONDS) === 0L)
+
+    assert(FileTime.from(1, MILLISECONDS).to(NANOSECONDS) === 1000000L)
+    assert(FileTime.from(1, MILLISECONDS).to(MICROSECONDS) === 1000L)
+    assert(FileTime.from(1, MILLISECONDS).to(MILLISECONDS) === 1L)
+    assert(FileTime.from(1, MILLISECONDS).to(SECONDS) === 0L)
+
+    assert(FileTime.from(1, SECONDS).to(NANOSECONDS) === 1000000000L)
+    assert(FileTime.from(1, SECONDS).to(MICROSECONDS) === 1000000L)
+    assert(FileTime.from(1, SECONDS).to(MILLISECONDS) === 1000L)
+    assert(FileTime.from(1, SECONDS).to(SECONDS) === 1L)
+    assert(FileTime.from(1, SECONDS).to(MINUTES) === 0L)
+
+    assert(FileTime.from(1, MINUTES).to(NANOSECONDS) === 60L * 1000000000L)
+    assert(FileTime.from(1, MINUTES).to(MICROSECONDS) === 60L * 1000000L)
+    assert(FileTime.from(1, MINUTES).to(MILLISECONDS) === 60L * 1000L)
+    assert(FileTime.from(1, MINUTES).to(SECONDS) === 60L)
+    assert(FileTime.from(1, MINUTES).to(MINUTES) === 1L)
+    assert(FileTime.from(1, MINUTES).to(HOURS) === 0L)
+
+    assert(FileTime.from(1, HOURS).to(NANOSECONDS) === 60L * 60L * 1000000000L)
+    assert(FileTime.from(1, HOURS).to(MICROSECONDS) === 60L * 60L * 1000000L)
+    assert(FileTime.from(1, HOURS).to(MILLISECONDS) === 60L * 60L * 1000L)
+    assert(FileTime.from(1, HOURS).to(SECONDS) === 60L * 60L)
+    assert(FileTime.from(1, HOURS).to(MINUTES) === 60L)
+    assert(FileTime.from(1, HOURS).to(HOURS) === 1L)
+    assert(FileTime.from(1, HOURS).to(DAYS) === 0L)
+
+    assert(FileTime.from(1, DAYS).to(NANOSECONDS) === 24L * 60L * 60L * 1000000000L)
+    assert(FileTime.from(1, DAYS).to(MICROSECONDS) === 24L * 60L * 60L * 1000000L)
+    assert(FileTime.from(1, DAYS).to(MILLISECONDS) === 24L * 60L * 60L * 1000L)
+    assert(FileTime.from(1, DAYS).to(SECONDS) === 24L * 60L * 60L)
+    assert(FileTime.from(1, DAYS).to(MINUTES) === 24L * 60L)
+    assert(FileTime.from(1, DAYS).to(HOURS) === 24L)
+    assert(FileTime.from(1, DAYS).to(DAYS) === 1L)
+    assert(FileTime.from(365, DAYS).to(DAYS) === 365L)
+  }
+
+  test("fromMillis(Long)") {
+    assert(FileTime.fromMillis(1).toMillis === 1)
+    assert(FileTime.fromMillis(0).toMillis === 0)
+  }
+
+  test("compareTo") {
+    val zero1 = FileTime.fromMillis(0)
+    val zero2 = FileTime.fromMillis(0)
+    val one   = FileTime.fromMillis(1)
+    val two   = FileTime.fromMillis(2)
+
+    assert(zero1.compareTo(zero2) === 0)
+    assert(zero2.compareTo(zero1) === 0)
+
+    assert(zero1.compareTo(one) < 0)
+    assert(one.compareTo(zero1) > 0)
+
+    assert(zero1.compareTo(two) < 0)
+    assert(two.compareTo(zero1) > 0)
+
+    assert(one.compareTo(two) < 0)
+    assert(two.compareTo(one) > 0)
+  }
+
+  test("equals") {
+    val zero1 = FileTime.fromMillis(0)
+    val zero2 = FileTime.fromMillis(0)
+    val one   = FileTime.fromMillis(1)
+
+    // Reflexive
+    assert(zero1 == zero1)
+    assert(zero2 == zero2)
+    assert(one == one)
+
+    // Symmetric
+    assert(zero1 == zero2)
+    assert(zero2 == zero1)
+    assert(one != zero1)
+    assert(zero1 != one)
+
+    // Transitive
+    val zero3 = FileTime.fromMillis(0)
+    assert(zero1 == zero2 && zero2 == zero3 && zero3 == zero1)
+  }
+
+  test("hashCode") {
+    val zero1 = FileTime.fromMillis(0)
+    val zero2 = FileTime.fromMillis(0)
+    val one   = FileTime.fromMillis(1)
+
+    // Reflexive
+    assert(zero1.hashCode() == zero1.hashCode())
+    assert(zero2.hashCode() == zero2.hashCode())
+    assert(one.hashCode() == one.hashCode())
+
+    // Symmetric
+    assert(zero1.hashCode() == zero2.hashCode())
+    assert(one.hashCode() != zero1.hashCode())
+
+    // Transitive
+    val zero3 = FileTime.fromMillis(0)
+    assert(zero1 == zero2 && zero2 == zero3 && zero3 == zero1)
+  }
+
+  test("toString") {
+    assert(FileTime.fromMillis(0).toString === "1970-01-01T00:00:00Z")
+    assert(FileTime.fromMillis(1).toString === "1970-01-01T00:00:00.001Z")
+    assert(FileTime.fromMillis(1234567890123L).toString === "2009-02-13T23:31:30.123Z")
+  }
+
+  test("to(TimeUnit)") {
+    assert(FileTime.fromMillis(1234567890L).to(NANOSECONDS) === 1234567890000000L)
+    assert(FileTime.fromMillis(1234567890L).to(MICROSECONDS) === 1234567890000L)
+    assert(FileTime.fromMillis(1234567890L).to(MILLISECONDS) === 1234567890L)
+    assert(FileTime.fromMillis(1234567890L).to(SECONDS) === 1234567L)
+    assert(FileTime.fromMillis(1234567890L).to(MINUTES) === 20576L)
+    assert(FileTime.fromMillis(1234567890L).to(HOURS) === 342)
+    assert(FileTime.fromMillis(1234567890L).to(DAYS) === 14)
+  }
+
+  test("toMillis") {
+    assert(FileTime.fromMillis(1000).toMillis === 1000L)
+    assert(FileTime.fromMillis(1000000).toMillis === 1000000L)
+  }
+}
